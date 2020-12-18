1d36017854bf7d45b684bd6528a605d1d98e3add
sandip rijal <sandurijal03@hotmail.com>
2020-12-19 02:50:46 +0545

    basics react router setup for home, events, bookings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>Hello world</h1>
-    </div>
-  );
+import './App.css';
+import AuthPage from './pages/Auth';
+import Bookings from './pages/Bookings';
+import Events from './pages/Events';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Redirect exact from='/' to='/auth' />
+          <Route path='/auth' component={AuthPage} />
+          <Route exact path='/events' component={Events} />
+          <Route exact path='/bookings' component={Bookings} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Bookings extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Bookings</h1>
+      </div>
+    );
+  }
+}
